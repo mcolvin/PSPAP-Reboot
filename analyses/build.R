ebde7b6c32835a85557e8d351ea36cebaa20cedf
mcolvin <colvin.mike@gmail.com>
2016-11-14 09:48:06 -0600

    retooled index
    retooled index for titles and toc

--- analyses/build.R
+++ analyses/build.R
@@ -1,7 +1,18 @@
 
 setwd('C:/Users/mcolvin/Documents/projects/Pallid Sturgeon/Analysis/PSPAP-Reboot/analyses')
+
+
 # RENDER SITE
 rmarkdown::render_site()# build website
+dir()
+
+junk <- c(dir(path=getwd(), pattern=c("inp")),
+    dir(path=getwd(), pattern=c("vcv")),
+    dir(path=getwd(), pattern=c("out")),
+    dir(path=getwd(), pattern=c("res")))
+if(length(junk)>0){file.remove(junk) }
+
+
 # COPY FILES TO DOCS FOR GITHUB.IO
 system(paste("xcopy", 
     '"C:/Users/mcolvin/Documents/projects/Pallid Sturgeon/Analysis/PSPAP-Reboot/analyses/_site"', 
