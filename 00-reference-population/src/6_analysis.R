608a2fd9db9dc2dbb33899d7751fdeb772759452
mcolvin <colvin.mike@gmail.com>
2017-05-19 16:08:54 -0500

    POST MEETING COMMIT

--- 00-reference-population/src/6_analysis.R
+++ 00-reference-population/src/6_analysis.R
@@ -1,6 +1,7 @@
 
 segs<- c(1,2,3,4,7,8,9,11,10,13,14)
 nyears<- 20
+
 beta0<- 2.9444
 phi<-matrix(plogis(beta0),length(segs),nyears-1)
 
@@ -13,6 +14,22 @@ sim_pop<-reference_population(segs=segs,
     phi=phi) # MATRIX OF YEAR TO YEAR AND SEGEMENT SPECIFIC SURVIVALS
 
     
-    
-    
-    
+head(sim_pop)  
+
+f<- 200 # trammel
+q<- 0.01
+N<-sim_pop[1,1]  
+
+C<- f*q*N    
+C
+C/f
+
+
+# CATCH EFFORT
+p<-0.05 # capture p
+
+catch<- rbinom(1,N,p)
+ind<- rep(1,N)
+
+ch<- rbinom(N,1,p)
+ch<- cbind(ch,  rbinom(N,1,p))
