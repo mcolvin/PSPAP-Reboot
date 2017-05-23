94b958faeecce92c933f0769c14681886fc5f3d0
sreynolds2 <s-sreynol5@math.unl.edu>
2017-05-23 14:20:00 -0500

    Effort Table Code
    Output Effort Table from 01-PSPAP-Background and add to dat of
    00-reference-population to be read as input.

--- 00-reference-population/src/3_load-and-clean.R
+++ 00-reference-population/src/3_load-and-clean.R
@@ -1,8 +1,16 @@
 
 # READ IN BEND DATA FOR RPMA 2 (UPPER) AND 4 (LOWER)
 
-bends<- read.csv("./dat/bend-data.csv")
+  bends<- read.csv("./dat/bend-data.csv")
 
-# make data.frame column names lower case
-names(bends)<- tolower(names(bends))
-bends<- subset(bends,b_segment %in% c(1,2,3,4,7,8,9,10,13,14))
+  # make data.frame column names lower case
+  names(bends)<- tolower(names(bends))
+  bends<- subset(bends,b_segment %in% c(1,2,3,4,7,8,9,10,13,14))
+
+
+
+# READ IN EFFORT DATA FROM 01-PSPAP-Background Analysis
+
+  effort<- read.table("./dat/effort_dat.csv")
+
+
