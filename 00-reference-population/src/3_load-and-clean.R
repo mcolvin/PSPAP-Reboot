b72a7cfb646ffd377b8cf578e9d6256a1febc3dd
sreynolds2 <s-sreynol5@math.unl.edu>
2017-05-25 13:30:02 -0500

    Clean Effort Table
    Adjusted effort to use only particular standard gears (based on sampling
    document) and the same standard gears for each basin.

--- 00-reference-population/src/3_load-and-clean.R
+++ 00-reference-population/src/3_load-and-clean.R
@@ -9,8 +9,15 @@
 
 
 
-# READ IN EFFORT DATA FROM 01-PSPAP-Background Analysis
-
+  # READ IN EFFORT DATA FROM 01-PSPAP-Background Analysis
+  
   effort<- read.table("./dat/effort_dat.csv")
+  
+  #Report Effort for GN14,GN18,GN41, GN81, MF, OT16, TLC1, TLC2, TN for both LB and UB
+  effort<-rbind(effort[1:8,],effort[12:13,],effort[2,], effort[14,], effort[4,],effort[15:18,],effort[20,])
+  effort[11,1]<-"UB"
+  effort[13,1]<-"UB"
+  effort[11,4:11]<-as.integer(rep(0,8))
+  effort[13,4:11]<-as.integer(rep(0,8))
 
 
