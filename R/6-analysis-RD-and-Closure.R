f77748c5085a6886166166bdc91cfb68b07842e9
mcolvin <colvin.mike@gmail.com>
2017-07-19 08:08:33 -0500

    fixing random temp emigration

--- R/6-analysis-RD-and-Closure.R
+++ R/6-analysis-RD-and-Closure.R
@@ -7,98 +7,13 @@ sim_ch<-function(inputs,...)
     Z<- matrix(0,inputs$n,inputs$nprim)
     Z_inn<-Z
     Z_out<-Z
-
-    Z[,1]<-1
-    Z_inn[1:inputs$n_inn,1]<-1
-    Z_out[,1]<- 1*(1-Z_inn[,1])
-
-    # SIMULATE CAPTURE HISTORY
-    ## MORE THAN 1 CAPTURE OCCASIONS
-    ch<- matrix(0,nrow(Z),sum(inputs$nsec))
-
-    ## LINK SECONDARY OCCASION TO PRIMARY OCCASION
-    #primsec<- matrix(
-    #    cbind(rep(1:inputs$nprim,inputs$nsec),
-    #    c(1:sum(inputs$nsec))), ncol=2)
-
-     
-    ## CAPTURE HISTORY FOR T=1    
-    ## UPDATE WHO IS IN AND OUT
-    ## AFTER DAY TO DAY MOVEMENT
-    i<-1       
-    ch<-Z_inn2<-Z_out2<- c()
-    for(k in 1:inputs$nsec[i])
-        {
-        ch<- cbind(ch,rbinom(nrow(Z_inn),1,inputs$p[i]*Z_inn[,i]*Z[,i]))    
-        ## MOVEMENT OUT OF STUDY AREA
-        innToOut<- rbinom(n=nrow(Z),
-            size=1,
-            prob=inputs$gam_d_prime2[i]*Z[,i]*Z_inn[,i])
-        innToInn<- (1-innToOut)*(Z[,i]*Z_inn[,i])
-        ## REMAINING OUT OF STUDY AREA
-        outToOut<- rbinom(n=nrow(Z),
-            size=1,
-            prob=inputs$gam_prime2[i]*Z[,i]*Z_out[,i])
-        outToInn<-  (1-outToOut)*(Z[,i]*Z_out[,i])
-        Z_inn[,i]<-0 
-        Z_inn[which(innToInn==1 | outToInn==1),i]<- 1
-        Z_out[,i]<-0 
-        Z_out[which(outToOut==1 | innToOut==1),i]<- 1
-        Z_inn2<- cbind(Z_inn2,Z_inn[,i])
-        Z_out2<- cbind(Z_out2,Z_out[,i])
-        }    
-
-     
-
+    Z[,1]<-1   
     ## SIMULATE POPULATION DYNAMICS
     for(i in 2:inputs$nprim)
-        {
-        ## SURVIVAL
+        {## SURVIVAL
         Z[,i]<- rbinom(n=nrow(Z),
             size=1,
             prob=inputs$phi[i-1]*Z[,i-1])
-        
-        ## MOVEMENT OUT OF STUDY AREA
-        innToOut<- rbinom(n=nrow(Z),
-            size=1,
-            prob=inputs$gam_d_prime[i-1]*Z[,i]*Z_inn[,i-1])
-        innToInn<- (1-innToOut)*(Z[,i]*Z_inn[,i-1])
-        
-        ## REMAINING OUT OF STUDY AREA
-        outToOut<- rbinom(n=nrow(Z),
-            size=1,
-            prob=inputs$gam_prime[i-1]*Z[,i]*Z_out[,i-1])
-        outToInn<-  (1-outToOut)*(Z[,i]*Z_out[,i-1])
-        Z_inn[,i]<-0 
-        Z_inn[which(innToInn==1 | outToInn==1),i]<- 1
-        Z_out[,i]<-0 
-        Z_out[which(outToOut==1 | innToOut==1),i]<- 1
-
-       
-        ## SIMULATE DAY TO DAY MOVEMENT AND
-        ## CAPTURE HISTORY    
-        for(k in 1:inputs$nsec[i])
-            {
-            ch<- cbind.fill(ch,
-                rbinom(nrow(Z_inn),1,inputs$p[i]*Z_inn[,i]*Z[,i]))        
-            ## MOVEMENT OUT OF STUDY AREA
-            innToOut<- rbinom(n=nrow(Z),
-                size=1,
-                prob=inputs$gam_d_prime2[i]*Z[,i]*Z_inn[,i])
-            innToInn<- (1-innToOut)*(Z[,i]*Z_inn[,i])
-            ## REMAINING OUT OF STUDY AREA
-            outToOut<- rbinom(n=nrow(Z),
-                size=1,
-                prob=inputs$gam_prime2[i]*Z[,i]*Z_out[,i])
-            outToInn<-  (1-outToOut)*(Z[,i]*Z_out[,i])
-            Z_inn[,i]<-0 
-            Z_inn[which(innToInn==1 | outToInn==1),i]<- 1
-            Z_out[,i]<-0 
-            Z_out[which(outToOut==1 | innToOut==1),i]<- 1
-            Z_inn2<- cbind.fill(Z_inn2,Z_inn[,i])
-            Z_out2<- cbind.fill(Z_out2,Z_out[,i])
-            }  
-        
         ## RECRUITMENT
         ## ASSUME RECRUITS SETTLE OUTSIDE OF 
         ## STUDY AREA
@@ -111,11 +26,70 @@ sim_ch<-function(inputs,...)
         Z_inn<- rbind(Z_inn,app)
         }
 
+        
+    if(movement="random")
+        {
+        Z_inn<- rbn
+        Z_out<- (1-Z_inn)*Z[,i]
+        
+        }
+    if(movement="markovian")
+        {
+        ## SIMULATE MOVEMENT
+        ## NOTE...... RANDOM DOES NOT DEPEND ON THE PREVIOUS
+        ## LOCATION, I HAVE BEEN IMPLEMENTING MARKOVIAN BECAUSE
+        ## GAMMAS DEPEND ON PREVIOUS LOCATION.
+        occ<- rep(1:inputs$nprim,inputs$nsec)
+        for(i in 1:inputs$nprim)
+            {
+            if(i==1)
+                {
+                Z_inn<-as.matrix(c(rep(1,inputs$n_inn),rep(0,inputs$n-inputs$n_inn)),ncol=1)
+                Z_out<- 1-Z_inn             
+                }
+            if(i>1)
+                {
+                ## MOVEMENT OUT OF STUDY AREA
+                innToOut<- rbinom(n=nrow(Z),
+                    size=1,
+                    prob=inputs$gam_d_prime[i]*Z[,i]*Z_inn[,ncol(Z_inn)-1])
+                innToInn<- (1-innToOut)*(Z[,i]*Z_inn[,ncol(Z_inn)-1])
+                ## REMAINING OUT OF STUDY AREA
+                outToOut<- rbinom(n=nrow(Z),
+                    size=1,
+                    prob=inputs$gam_prime[i]*Z[,i]*Z_out[,ncol(Z_inn)])
+                outToInn<-  (1-outToOut)*(Z[,i]*Z_out[,ncol(Z_inn)])
+                Z_inn<-cbind(Z_inn,innToInn+outToInn)
+                Z_out<-cbind(Z_out,outToOut+innToOut)
+                }
+            for(j in 2:inputs$nsec[i])
+                {
+                ## MOVEMENT OUT OF STUDY AREA
+                innToOut<- rbinom(n=nrow(Z),
+                    size=1,
+                    prob=inputs$gam_d_prime2[i]*Z[,i]*Z_inn[,ncol(Z_inn)])
+                innToInn<- (1-innToOut)*(Z[,i]*Z_inn[,ncol(Z_inn)])
+                ## REMAINING OUT OF STUDY AREA
+                outToOut<- rbinom(n=nrow(Z),
+                    size=1,
+                    prob=inputs$gam_prime2[i]*Z[,i]*Z_out[,ncol(Z_inn)])
+                outToInn<-  (1-outToOut)*(Z[,i]*Z_out[,ncol(Z_inn)])
+                
+                Z_inn<-cbind(Z_inn,innToInn+outToInn)
+                Z_out<-cbind(Z_out,outToOut+innToOut)          
+                }
+            }
+        }   
 
-        
-        
-
-        
+    ## SIMULATE CAPTURE HISTORIES
+    ch<- matrix(0,nrow(Z_inn),ncol(Z_inn))
+    for(i in 1:ncol(Z_inn2))
+        {
+        ch[,i]<-rbinom(nrow(Z_inn),1,inputs$p[occ[i]]*Z_inn[,i])
+        }
+    
+             
+                   
     ## SUBSET OUT FISH THAT ARE NEVER CAPTURED
     ch<- ch[which(apply(ch,1,sum)!=0),]
     ## NEED THIS FOR RMARK
@@ -177,4 +151,88 @@ est_rd<-function(inputs,...)
     }
 
 
+sim_rd_ch<-function(n=100, 
+    n_unobservable=200, 
+    nprim=5,
+    nsec=3,
+    phi=0.8,
+    gamma_prime=0.3,
+    gamma_dblprime=0.3,
+    p=0.3,  
+    ...)
+    {
+    
+    super_n<- n+n_unobservable # SUPER POPULATION (ONSITE + OFFSITE)
+    Z<- matrix(0,super_n,nprim)
+    Z_observable<- matrix(0,super_n,nprim)
+    Z_unobservable<- matrix(0,super_n,nprim)
+    Z[1:super_n,1]<-1
+    Z_observable[1:n,1]<-1
+    Z_unobservable[(n+1):(n+n_unobservable),1]<-1
 
+
+    # SURVIVAL,, EMIGRATION, AND MIGRATION
+    for(i in 1:super_n) # FOR EACH INDIVIDUAL IN THE SUPER POPULATION
+        {
+        for(j in 2:(nprim)) # FOR PRIMARY OCCASIONS 2:NPRIM
+            {
+            # DOES A FISH SURVIVE, IF IT WAS PREVIOUSLY ALIVE?
+            Z[i,j]<- rbinom(1,1,phi[j-1])*Z[i,j-1]
+
+            prs<-c(
+                # OBSERVABLE[t-1] --> UNOBSERVABLE[t]
+                (1-Z_unobservable[i,j-1])*(gamma_dblprime[j-1]),
+                
+                # OBSERVABLE[t-1] --> OBSERVABLE[t]
+                (1-Z_unobservable[i,j-1])*(1-gamma_dblprime[j-1]),
+                
+                # UNOBSERVABLE[t-1]--> UNOBSERVABLE[t]
+                Z_unobservable[i,j-1]*gamma_prime[j-1],
+                
+                # UNOBSERVABLE[t-1]--> 	OBSERVABLE[t]
+                Z_unobservable[i,j-1]*(1-gamma_prime[j-1]))
+
+            state<-sample(1:4,1,prs,replace=FALSE)
+            if(state==1 & Z[i,j]==1){Z_unobservable[i,j]<- 1}
+            if(state==2 & Z[i,j]==1){Z_observable[i,j]<-1}
+            if(state==3 & Z[i,j]==1){Z_unobservable[i,j]<-1}
+            if(state==4 & Z[i,j]==1){Z_observable[i,j]<-1}
+            }
+        }
+        
+        
+        
+        
+    # SIMULATE CAPTURE HISTORY
+    ## MORE THAN 1 CAPTURE OCCASIONS
+    ch<- matrix(0,nrow(Z),sum(nsec))
+    avail2Capture<-ch
+    ## LINK SECONDARY OCCASION TO PRIMARY OCCASION
+    primsec<- matrix(cbind(rep(1:nprim,nsec),
+        c(1:sum(nsec))), ncol=2)
+    ## CAPTURE HISTORY CONDITIONAL ON BEING PRESENT (I.E., NOT EMIGRATED)
+    for(i in 1:super_n)
+        {
+        for(j in 1:sum(nsec))
+            {
+            primocc<- primsec[j,1]
+            avail2Capture[i,j]<-Z_observable[i,primsec[j,1]]
+            ch[i,j]<- rbinom(1,1,p[primocc]*avail2Capture[i,j])
+            }
+        }
+        
+    ## SUBSET OUT FISH THAT ARE NEVER CAPTURED
+    ch<- ch[which(apply(ch,1,sum)!=0),]
+
+    ## NEED THIS FOR RMARK
+    ch2<- data.frame(ch=apply(ch,1,paste0,collapse=""),freq=1,stringsAsFactors=FALSE)# prep data for processing
+
+    # NEED TO MAKE A MATRIX OF THAT INDICATES THE FIRST OCCASION
+    ends<-cumsum(nsec) # last sampling occasion
+    occs<- rep(0,sum(nsec))
+    occs[ends]<-1# last occasion in primary
+    occs<- occs[-length(occs)]# drop last 1 for processing 
+
+
+    return(list(ch=ch2,occs=occs))        
+    }
