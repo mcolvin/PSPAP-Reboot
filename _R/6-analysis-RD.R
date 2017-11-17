c52ae4baef4737b7d9e298107cf1f7397755170f
sreynolds2 <sara.reynolds@huskers.unl.edu>
2017-11-17 08:36:55 -0600

    crunch updates

--- _R/6-analysis-RD.R
+++ _R/6-analysis-RD.R
@@ -4,9 +4,9 @@ source("_R/1_global.R")
 source("_R/2_functions.R")
 source("_R/3_load-and-clean.R")
 
-#############################################################################
-# 1-2 HAS OCC 3 ONLY; 1-2 Occ3, 2-3 all, & 2-1 all, do not have "fit" saved #
-#############################################################################
+#########################################################
+# 1-2 Occ3, 2-3 all, & 2-1 all, do not have "fit" saved #
+#########################################################
 
 
 if(pcname=="WF-FGNL842")
@@ -18,9 +18,9 @@ if(pcname!="WF-FGNL842")
   loc<-"D:/"
 }
 ptm<-proc.time()
-lapply(1:400, function(i)
+lapply(3:100, function(i)
 {
-  lapply(1:4, function(j)
+  lapply(3:4, function(j)
   {
     # SET OCCASIONS TO BE USED
     occasions<-2:3
