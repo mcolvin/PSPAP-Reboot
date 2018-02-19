0cf2c900bef75b119c0ed41993841060385af1af
sreynolds2 <s-sreynol5@math.unl.edu>
2018-02-19 07:16:50 -0600

    values file and fig

---
+++ _R/6-analysis-values-table.R
@@ -0,0 +1,43 @@
+library(plyr)
+tt<-readRDS("_output/4-tables/trnd_table.rds")
+tt<-tt[,c("pop_id","catch_id","samp_type", "occasions", "gear", "estimator", "q_mean_input",
+          "B0_sd_input", "q_mean_realized", "q_sd_realized", "recruits_UB", "r_years_UB", 
+          "recruits_LB", "r_years_LB", "exp_bias", "exp_precision")]
+names(tt)[15]<-"trend_bias"
+names(tt)[16]<-"trend_precision"
+
+tt2<-ddply(tt, .(samp_type, occasions, gear, estimator), summarize,
+           mean_trend_bias=mean(trend_bias, na.rm = TRUE),
+           mean_trend_precision=mean(trend_precision, na.rm = TRUE))
+tt2<-tt2[which(tt2$gear %in% c("GN14", "TLC1", "TN")),]
+
+dat_list<-dir("_output/4-tables/", ".rds")
+dat_list<-dat_list[c(1:7)]
+tbl2<-lapply(dat_list, function(x)
+{
+  dat<-readRDS(paste0("_output/4-tables/", x))
+  
+  
+  tmp<-ddply(dat, 
+             .(samp_type, occasions, gear, estimator), 
+             summarize, 
+             abundance_bias=mean(rel_bias, na.rm=TRUE), 
+             abundance_precision=mean(prec, na.rm=TRUE))
+  return(tmp)
+})
+tbl2<-do.call(rbind,tbl2)
+
+tbl2<-tbl2[which(tbl2$gear %in% c("GN14", "TLC1", "TN")),]
+
+final2<-merge(tt2, tbl2, 
+              by=c("samp_type", "occasions", "gear", "estimator"), 
+              all=TRUE)
+final2<-final2[order(final2$samp_type,final2$occasions, final2$estimator),]
+finalGN<-final2[which(final2$gear=="GN14"),]
+finalTL<-final2[which(final2$gear=="TLC1"),]
+finalTN<-final2[which(final2$gear=="TN"),]
+
+
+write.csv(finalGN, "_output/values_GN.csv", row.names = FALSE)
+write.csv(finalTL, "_output/values_TL.csv", row.names = FALSE)
+write.csv(finalTN, "_output/values_TN.csv", row.names = FALSE)
